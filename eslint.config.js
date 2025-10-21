14ca915b46df58f05d8148f421414b71713a3a70
sdp-maker <19179977082@163.com>
2025-10-21 18:57:00 +0800

    chore: 更新依赖文件和配置
    更新了多个依赖文件和配置，包括新增的字典文件、模块类型声明和构建配置。主要涉及npm模块的安装和更新，以及相关工具链的调整。

---
+++ eslint.config.js
@@ -0,0 +1,67 @@
+/*
+ * @Author: sdp-maker 19179977082@163.com
+ * @Date: 2025-10-21 17:46:31
+ * @LastEditors: sdp-maker 19179977082@163.com
+ * @LastEditTime: 2025-10-21 18:14:28
+ * @FilePath: \LowCode\eslint.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import parserVue from 'vue-eslint-parser'
+import pluginImportSort from 'eslint-plugin-simple-import-sort'
+
+
+
+export default [
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            globals: globals.browser,
+            ecmaVersion: 'latest',
+            sourceType: 'module'
+        },
+        rules: {
+            ...js.configs.recommended.rules
+        }
+    },
+    {
+        files: ['**/*.vue'],
+        languageOptions: {
+            parser: parserVue,
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                ecmaVersion: 'latest',
+                sourceType: 'module'
+            },
+            globals: {...globals.browser, 
+                computed: 'readonly',
+                reactive: 'readonly',
+                ref: 'readonly',
+                onMounted: 'readonly',
+                onUpdated: 'readonly',
+                onUnmounted: 'readonly',
+                onBeforeMount: 'readonly',
+                onBeforeUpdate: 'readonly',
+                onAfterUpdate: 'readonly',
+                onBeforeUnmount: 'readonly',
+                onActivated: 'readonly',
+                onDeactivated: 'readonly',
+                onErrorCaptured: 'readonly',
+                defineExpose: 'readonly',
+                watch: 'readonly',
+                watchEffect: 'readonly',
+            }
+            
+        },
+        plugins: {
+            vue: pluginVue,
+            'simple-import-sort': pluginImportSort
+        },
+        rules: {
+            ...pluginVue.configs['flat/recommended'].rules,
+            "no-console": "error"
+        }
+    }
+]
