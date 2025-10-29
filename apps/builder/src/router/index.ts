7252d15b1ffd59a029c3f1fe65f32fbcb24f274f
sdp-maker <19179977082@163.com>
2025-10-29 22:51:14 +0800

    fix:切换hash模式

--- apps/builder/src/router/index.ts
+++ apps/builder/src/router/index.ts
@@ -6,7 +6,7 @@
  * @FilePath: \LowCode\apps\builder\src\router\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 
 import LayoutView from '@/views/LayoutView.vue'
 import DataSourceView from '@/views/DataSourceView.vue'
@@ -14,7 +14,9 @@ import ActionsView from '@/views/ActionsView.vue'
 import AppView from '@/components/AppNavigator.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // 临时使用 Hash 模式避免刷新 404 问题
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
