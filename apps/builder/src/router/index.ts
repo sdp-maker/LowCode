9fb4c6fe08053c689645e1481e571ee23e582254
sdp-maker <19179977082@163.com>
2025-10-29 23:05:57 +0800

    fix:bug修复

--- apps/builder/src/router/index.ts
+++ apps/builder/src/router/index.ts
@@ -6,7 +6,7 @@
  * @FilePath: \LowCode\apps\builder\src\router\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import LayoutView from '@/views/LayoutView.vue'
 import DataSourceView from '@/views/DataSourceView.vue'
